774253ad7f93e70a517f09f7c208908aac8b5926
hessuew <juhanijuusola@gmail.com>
2025-03-13 05:17:27 +0200

    refactor: remove old images, new favicons and image structure

---
+++ src/data/ministers.ts
@@ -0,0 +1,100 @@
+// import ahunsi from '~/assets/images/elders/ahunsi.webp';
+import akosua from '~/assets/images/deacons/akosua.webp';
+import ben from '~/assets/images/deacons/ben.webp';
+import ida from '~/assets/images/deacons/ida.webp';
+import juhani_helsinki from '~/assets/images/elders/juhani_helsinki.webp';
+// import cj from '~/assets/images/elders/cj.webp';
+import greta from '~/assets/images/greta.jpg';
+
+// import mahi from '~/assets/images/elders/mahi.webp';
+
+// {
+//   title: 'Paimen',
+//   name: 'Mahidere Ali',
+//   image: {
+//     src: mahi,
+//     alt: 'Mahidere - kuva',
+//   },
+//   isReversed: true,
+// },
+// {
+//   title: 'Paimen',
+//   name: 'CJ',
+//   image: {
+//     src: cj,
+//     alt: 'CJ - kuva',
+//   },
+//   isReversed: false,
+// },
+// {
+//   title: 'Paimen',
+//   name: 'Ahunsi Adeniran',
+//   image: {
+//     src: ahunsi,
+//     alt: 'Ahunsi - kuva',
+//   },
+//   isReversed: false,
+// },
+
+export const ministers = [
+  {
+    // <br /> <br />
+    // 2) Nyt ymmärsin sen. Jumala sanoi Jeesukselle, 'Tämä on rakas poikani johon olen mieltynyt'. Sananlaskut 16:7 sanoo, 'Kun miehen tiet miellyttävät Herraa, Hän saataa hänen vihollisensa hänen ystävikseen'. Eivät fariseukset ja kansa rakastaneet ja halanneet Jeesusta vaan ristiinnaulitsivat Hänet, 1 Kor 2:8, 'Jos he olisivat tietäneet he eivät olisi ristiinnaulinneet kirkkauden Herraa'. Eli kun sinun tiesi miellyttää Jumalaa Hän saattaa sinun vihollistesi kaikkien tekojen sinua kohtaan olevan kuin ystävän palveluksia! Eli vaikka kuinka he vainoaisivat sinua tai vihaisivat sinua, kaikki mitä he tekevät, on kuin ystävän tekoja sinulle! Vau! Kiitos Jeesus!!
+    title: 'Valvoja (Piispa)',
+    name: 'Juhani Juusola',
+    content: `1) Tämä on Herran sana: tuo henkilö joutuu lähtemään pois työpaikastasi. Hän ei pysty kestämään tätä Jumalan tulta sinussa.
+    <br /> <br />
+    2) Sinut on kutsuttu, Jumalan kirkkaus on sinun päälläsi. Ole uskollinen siinä vähässä, mitä sinulle on annettu äläkä anna mitään tekosyitä. Jumala on asettanut sinut kämmenelleen käyttääkseen sinua ja Hän tulee kasvattamaan sinua. Kyse ei ole sinun kirkkaudestasi vaan Jeesuksen nimen ja tuomisestä esille. Älä laita kutsuasi sivuun edes hetkeksi, vaan tee siitä varma ja selkeä, sillä Jumala tulee kirkastumaan elämäsi kautta.`,
+    image: {
+      src: juhani_helsinki,
+      alt: 'Juhani - kuva',
+    },
+    isReversed: false,
+  },
+  {
+    title: 'Paimen',
+    name: 'Greta Juusola',
+    content: `1) *****n argumentti: ‘Mutta sinä et ymmärrä, kun sinulla ei ole lapsia.’ 
+      Gretan vastaus: ’Ei olekaan, mutta minä tiedän mitä Raamattu sanoo ja Raamattu sanoo että kädenlämpöisyys ja että jos mikään asia on tärkeämpi kuin Jumala, niin se on väärin.’
+      <br /> <br />
+      2) Kun vaellamme Kristuksen tiellä täysin antautuneena, olemme täydellisiä niin kuin itse Kristus on. Niin kauan kun emme katso lihaan, Jumala kyllä ilmoittaa kaikesta miten asiat ovat. Fil. 3:12-16`,
+    image: {
+      src: greta,
+      alt: 'Rakas Greta - kuva',
+    },
+    isReversed: true,
+  },
+  {
+    title: 'Seurakunta Palvelija',
+    name: 'Akosua Osei',
+    content: `1) Ihmiset ilman Jumalan Henkeä sisimmässään olivat uskonnollisia. Siksi heidän puheensa ei tuonut synnintuntoa toisille. Stefanos taas oli mies täynnä Pyhää Henkeä. Tämä tarkoitti, että hänen suustaan lähtevät sanat toivat synnintunnon kuulijoiden sydämiin. Uskonnolliset ihmiset olisivat halunneet jonkun toisen puhuvan, koska silloin he olisivat voineet elää niin kuin elivät ilman synnintuntoa.
+              <br /> <br />
+              2) Raamattu komentaa meitä etsimään ja asettamaan mielemme niihin asioihin, jotka ovat ylhäällä ja ikuisia. Stefanos piti mielensä aina ikuisissa asioissa. Ja kun uskonnollinen väkijoukko alkoi kiristellä hampaitaan hänelle, Stefanos katsoi taivaaseen. Hän oli tottunut siihen, eikä hän keskittynyt ollenkaan ihmisten  reaktioihin. Hän oli toki täynnä Pyhää Henkeä ja Henki kykeni ohjaamaan hänen silmänsä ylös. Siitä huolimatta hän keskittyi itse ylhäällä oleviin asioihin.
+              <br /> <br />
+              3) Koska Stefanos oli niin tottunut keskittymään ikuisiin asioihin, kun hän tällä kertaa katsoi taivaaseen hän näki Jumalan kirkkauden ja Jeesuksen.`,
+    image: {
+      src: akosua,
+      alt: 'Iloinen Akosua - kuva',
+    },
+    isReversed: false,
+  },
+  {
+    title: 'Seurakunta Palvelija',
+    name: 'Ida Pikkarainen',
+    content: `1) Kun rukoilimme, näin kun Mahi oli verkossa. Monta kalastajan verkkoa oli kietoutunut hánen ympärilleen ja hän oli aivan jumissa. Hän oli sikiöasennossa ja ei pystynyt liikkumaan. Mutta sitten rukoillessamme verkon punokset alkoivat erkaantua toisistaan. Verkot hajosivat itsestään, kunnes niistä ei ollut jäljellä mitään. Sitten Mahi nousi ylös ja lähti liikkeelle niin iloisena Herran antamasta vapaudesta. Halleluja!!`,
+    image: {
+      src: ida,
+      alt: 'Riemukas Ida - kuva',
+    },
+    isReversed: true,
+  },
+  {
+    title: 'Seurakunta Palvelija',
+    name: 'Benwillis Onchonga',
+    image: {
+      src: ben,
+      alt: 'Benwillis - kuva',
+    },
+    isReversed: true,
+  },
+];
